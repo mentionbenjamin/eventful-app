501c9e8cdf277349057ea4d2416247ff17c3a516
Ben Robinson <mentionbenjamin@gmail.com>
2018-08-17 16:02:56 +0100

    adds html form

--- client/src/models/events.js
+++ client/src/models/events.js
@@ -13,19 +13,22 @@ Events.prototype.getData = function (townName) {
   const url = `https://geocode.xyz/${townName},UK?json=1`;
   const request = new Request(url);
   request.get()
-    .then((data) => {
-       const url = `https://www.skiddle.com/api/v1/events/search/?api_key=${apiKey.apiKey}&limit=100&eventcode=LIVE&order=goingto&latitude=${data.latt}&longitude=${data.longt}&radius=10`;
-       const request = new Request(url);
-       return request.get();
-    })
-    .then((data) => {
-      this.events = data.results;
-      PubSub.publish('Events:event-data-loaded', this.events);
-      console.log(this.events);
-    })
-   .catch((err) => {
-     console.error(err);
+  .then((data) => {
+    const url = `https://www.skiddle.com/api/v1/events/search/?api_key=${apiKey.apiKey}&limit=100&eventcode=LIVE&order=goingto&latitude=${data.latt}&longitude=${data.longt}&radius=10`;
+    const request = new Request(url);
+    return request.get();
+
+  })
+  .then((data) => {
+    this.events = data.results;
+
+    PubSub.publish('Events:event-data-loaded', this.events);
+    // console.log(this.events);
+  })
+  .catch((err) => {
+    console.error(err);
   });
 }
+
 
 module.exports = Events;
