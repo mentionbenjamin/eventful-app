412fafacd308909bd4bc3a0be43844b17fdf0d42
Gregor Cox <gregorcox@gmail.com>
2018-08-17 09:14:52 +0100

    adds get data method for api data

--- client/src/models/events.js
+++ client/src/models/events.js
@@ -1,1 +1,15 @@
-//// TODO:
+const RequestHelper = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Events = function () {
+  this.events = events;
+  this.towns = towns;
+}
+
+Events.prototype.getData = function () {
+  const requestHelper = new RequestHelper('https://www.skiddle.com/api/v1/events/search/?api_key=ad2b6e3c9f2cbbb0b736f407132c55b7');
+  requestHelper.get((data) => {
+    this.handleDataReady(data);
+    PubSub.publish('Events:events-data-ready', this.events);
+  });
+};
