6a2c1b254910361f1cb9952820097096738c4404
Gregor Cox <gregorcox@gmail.com>
2018-08-17 17:57:43 +0100

    can populate category list with categories

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,12 @@
 const Events = require('./models/events.js');
+const SelectView = require('./views/select_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-
-
+  const selectCategory = document.querySelector('select#category');
+  const selectView = new SelectView(selectCategory);
+  selectView.bindEvents();
 
   const events = new Events();
   events.getData('Edinburgh');
--- client/src/views/select_view.js
+++ client/src/views/select_view.js
@@ -1,1 +1,47 @@
-//TODO
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function (element) {
+  this.element = element;
+};
+
+
+
+SelectView.prototype.bindEvents = function () {
+  PubSub.subscribe('Events:event-data-loaded', (evt) => {
+    const eventCategories = evt.detail;
+    console.log(evt.detail);
+    const categoryNames = this.getCategoryNames(evt.detail);
+    console.log(categoryNames);
+    this.populate(categoryNames);
+  });
+
+  this.element.addEventListener('change', (evt) => {
+    const selectedIndex = evt.target.value;
+    PubSub.publish('SelectView:change', selectedIndex);
+  });
+};
+
+// SelectView.prototype.populate = function (events) {
+//   events.forEach((event, index) => {
+//     const option = document.createElement('option');
+//     option.textContent = event.EventCode;
+//     option.value = index;
+//     this.element.appendChild(option);
+//   });
+// };
+
+SelectView.prototype.populate = function (categories) {
+  for (category of categories) {
+    const option = document.createElement('option');
+    option.textContent = category;
+    this.element.appendChild(option);
+  }
+};
+
+SelectView.prototype.getCategoryNames = function (events) {
+  return events
+    .map(event => event.EventCode)
+    .filter((category, index, categories) => categories.indexOf(category) === index);
+};
+
+module.exports = SelectView;
