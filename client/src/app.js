c38e29076cca6b663462adcc89ba7f9bfb7a644a
jamos90 <jamos_98@hotmail.com>
2018-08-18 06:32:00 +0100

    map displaying on screen, but rendering through app

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,21 @@
 const Events = require('./models/events.js');
+const leaflet = require('leaflet');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
 
+  var myMap = L.map('mapid', {
+    center: [51.505, -0.09],
+    zoom: 10
+  })
+
+
+  console.log(myMap)
+
 
 
   const events = new Events();
-  events.getData('Edinburgh');
+  // events.getData('Edinburgh');
   // console.log(events.getData('Edinburgh'));
 });
