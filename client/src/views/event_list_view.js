369d9d4bffb9e9143fbca0c2cd13843c8c79edc8
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-18 10:27:15 +0100

    list container set up

--- client/src/views/event_list_view.js
+++ client/src/views/event_list_view.js
@@ -1,1 +1,33 @@
-//TODO
+const PubSub = require('../helpers/pub_sub.js');
+const EventItemView = require('event_item_view.js');
+
+const EventListView = function(listItem) {
+  this.element = listItem;
+};
+
+ListView.prototype.setupEventListeners = function() {
+  this.element.addEventListener('submit', function(evt) {
+    evt.preventDefault();
+  });
+  PubSub.subscribe('Events:form-submitted', (evt) => {
+    const items = evt.detail;
+    this.renderList(items);
+  });
+};
+
+ListView.prototype.renderList = function(items) {
+  this.emptyList();
+  items.forEach((item) => this.renderItem(item));
+};
+
+ListView.prototype.emptyList = function(items) {
+  this.element.innerHTML = '';
+};
+
+ListView.prototype.renderItem = function(item) {
+  const eventItemView = new EventItemView(item);
+  const li = eventItemView.createLi();
+  this.element.appendChild(li);
+};
+
+module.exports = ListView;
