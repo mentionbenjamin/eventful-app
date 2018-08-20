d1fabf1a60ea2e0e074af964db52d75eadb682b8
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-20 11:02:13 +0100

    date and description aded

--- client/src/views/event_map_detail_view.js
+++ client/src/views/event_map_detail_view.js
@@ -13,21 +13,31 @@ EventMapDetailView.prototype.bindEvents = function () {
 }
 
 EventMapDetailView.prototype.renderEventDetails = function (event) {
-  this.container.innerHTML = " ";
-  console.log("hello");
-  const eventName = document.createElement('h4');
+    this.container.innerHTML = " ";
+    console.log("hello");
+    const eventName = document.createElement('h4');
     eventName.textContent = `${event.eventName}`;
     this.container.appendChild(eventName);
-  const eventType = document.createElement('p');
+
+    const eventType = document.createElement('p');
     eventType.textContent = `${event.eventType}`;
     this.container.appendChild(eventType);
+
+    const date = document.createElement('p');
+    date.textContent = `${event.date}`;
+    this.container.appendChild(date);
 
     const description = document.createElement('p');
     description.textContent = `${event.description}`;
     this.container.appendChild(description);
-  const price = document.createElement('p');
-  price.textContent = `${event.price}`;
-  this.container.appendChild(price);
+
+    const price = document.createElement('p');
+    price.textContent = `${event.price}`;
+    this.container.appendChild(price);
+
+    const link = document.createElement('p');
+    link.textContent = `${event.link}`;
+    this.container.appendChild(link);
 
   return this.container;
 };
