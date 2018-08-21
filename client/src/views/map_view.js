0fa3afcd1d21c7d0407c54a6cb41d8df916c1c85
jamos90 <jamos_98@hotmail.com>
2018-08-21 15:21:36 +0100

    pre merge commit

--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -11,11 +11,11 @@ MapView.prototype.bindEvents = function () {
    this.setMapMarkers(evt.detail);
   });
 
-  PubSub.subscribe('Events:saved-event-list', (evt) =>{
-   this.createMapSaved(evt.detail);
-   this.setMapMarkersSaved(evt.detail);
-
-  });
+  // PubSub.subscribe('Events:saved-event-list', (evt) =>{
+  //  this.createMapSaved(evt.detail);
+  //  this.setMapMarkersSaved(evt.detail);
+  //
+  // });
 }
 
   MapView.prototype.createMap = function (eventData) {
@@ -31,18 +31,19 @@ MapView.prototype.bindEvents = function () {
     }).addTo(myMap);
   };
 
-  MapView.prototype.createMapSaved = function (eventData) {
-    document.getElementById('map-container').innerHTML = "<div id ='mapid'></div>"
-    const latt = eventData[0].lat;
-    const longt = eventData[0].longt;
-    myMap = L.map('mapid', {
-    center: [latt, longt],
-    zoom: 10
-  }).flyTo([latt,longt])
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
-  };
+
+  // MapView.prototype.createMapSaved = function (eventData) {
+  //   document.getElementById('map-container').innerHTML = "<div id ='mapid'></div>"
+  //   const latt = eventData[0].lat;
+  //   const longt = eventData[0].longt;
+  //   myMap = L.map('mapid', {
+  //   center: [latt, longt],
+  //   zoom: 10
+  // }).flyTo([latt,longt])
+  // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  //       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  //   }).addTo(myMap);
+  // };
 
 
 
@@ -74,33 +75,31 @@ MapView.prototype.bindEvents = function () {
     }
   };
 
-  MapView.prototype.setMapMarkersSaved = function (eventData) {
-
-
-    const eventInformation = eventData;
-    for(var i = 0; i <eventInformation.length; i++){
-    const markerLayer =  L.layerGroup().addTo(myMap);
-    const venueLat = eventInformation[i].lat;
-    const venueLongt = eventInformation[i].venue.longit;
-    const eventMarker = L.marker([venueLat, venueLongt],{
-      opacity: 1,
-      riseOnHover: true,
-      riseOffSet: 250
-    })
-    eventMarker.customId = Math.floor((Math.random() * 100) + 1);
-    eventMarker.venueName = eventInformation[i].venue;
-    // eventMarker.eventType = eventInformation[i].EventCode;
-    // eventMarker.eventName =eventInformation[i].eventname;
-    // eventMarker.linkURL  = eventInformation[i].link;
-    // eventMarker.eventImage = eventInformation[i].imageurl;
-    // eventMarker.price = eventInformation[i].entryprice;
-    // eventMarker.description = eventInformation[i].description;
-    // eventMarker.date = eventInformation[i].date;
-
-
-    eventMarker.addTo(markerLayer).on('click', onMapClick)
-    }
-  };
+  // MapView.prototype.setMapMarkersSaved = function (eventData) {
+  //   const eventInformation = eventData;
+  //   for(var i = 0; i <eventInformation.length; i++){
+  //   const markerLayer =  L.layerGroup().addTo(myMap);
+  //   const venueLat = eventInformation[i].lat;
+  //   const venueLongt = eventInformation[i].longt;
+  //   const eventMarker = L.marker([venueLat, venueLongt],{
+  //     opacity: 1,
+  //     riseOnHover: true,
+  //     riseOffSet: 250
+  //   })
+  //   eventMarker.customId = Math.floor((Math.random() * 100) + 1);
+  //   eventMarker.venueName = eventInformation[i].venue;
+  //   // eventMarker.eventType = eventInformation[i].EventCode;
+  //   // eventMarker.eventName =eventInformation[i].eventname;
+  //   // eventMarker.linkURL  = eventInformation[i].link;
+  //   // eventMarker.eventImage = eventInformation[i].imageurl;
+  //   // eventMarker.price = eventInformation[i].entryprice;
+  //   // eventMarker.description = eventInformation[i].description;
+  //   // eventMarker.date = eventInformation[i].date;
+  //
+  //
+  //   eventMarker.addTo(markerLayer).on('click', onMapClick)
+  //   }
+  // };
 
 
 
